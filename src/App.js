1279bef17c1495cfb2519233fc41159c814b390c
Nisheeth Bandaru <nisheeth@outlook.com>
2021-12-05 17:08:14 -0500

    Initial build with flask and react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time')
+      .then(resp => resp.json())
+      .then(data => {
+        setCurrentTime(data["time"]);
+      })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The current time is {currentTime}.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
